af54f7ac944cad76dff74adda8b6adae48d986aa
Daniel Gatti <dan.gatti@jax.org>
2025-05-07 14:48:56 -0400

    Added Chr X code

--- clean_genoprobs_by_col.R
+++ clean_genoprobs_by_col.R
@@ -1,6 +1,42 @@
 ################################################################################
 # Given a qtl2-style genoprobs object, 
 ################################################################################
+
+clean_one_chr = function(pr, thr) {
+
+      # Get the diplotype sums at each marker.
+      cs = apply(pr, 2:3, sum)
+    
+      # Convert the colSums to a logical matrix in which TRUE means that 
+      # the colSum is less than the threshold.
+      lt_thr = cs < thr
+
+      # Find markers which have at least one column with a sum below
+      # the threshold.
+      wh = which(colSums(lt_thr) > 0)
+
+      # Loop through the markers that have low diplotype probs,
+      # zero out the problem columns, and renormalize the probs at 
+      # each marker.
+      for(mkr in wh) {
+
+        # Get the columns which need to be set to 0.
+        cols2zero = which(lt_thr[,mkr])
+      
+        # Set the columns to zero.
+        pr[,cols2zero,mkr] = 0
+      
+        # Renormalize to make the sum of each row at this marker equal to 1.
+        pr[,,mkr] = pr[,,mkr] / rowSums(pr[,,mkr])
+
+      } # for(mkr)
+      
+      return(pr)
+
+} # clean_one_chr()
+
+
+
 # probs: qtl2-style genoprobs object. Names list containing 3 dimensional
 #        numeric arrays wiht samples in rows, diplotypes in columns, and 
 #        makers in slices.
@@ -20,34 +56,48 @@ clean_genoprobs_by_col = function(probs, thr = 1.0) {
   # Loop through each chromosome.
   for(chr in seq_along(probs)) {
 
-    print(paste('CHRR:', names(probs)[chr]))
+    print(paste('CHR:', names(probs)[chr]))
 
-    # Get the diplotype sums at each marker.
-    cs = apply(probs[[chr]], 2:3, sum)
+    if(attr(probs, 'is_x_chr')[chr] == TRUE) {
     
-    # Convert the colSums to a logical matrix in which TRUE means that 
-    # the colSum is less than the threshold.
-    lt_thr = cs < thr
-
-    # Find markers which have at least one column with a sum below
-    # the threshold.
-    wh = which(colSums(lt_thr) > 0)
-
-    # Loop through the markers that have low diplotype probs,
-    # zero out the problem columns, and renormalize the probs at 
-    # each marker.
-    for(mkr in wh) {
-
-      # Get the columns which need to be set to 0.
-      cols2zero = which(lt_thr[,mkr])
+      # Figure out which samples are male and female.
+      y_cols = grep('Y$', colnames(probs[[chr]]))
       
-      # Set the columns to zero.
-      probs[[chr]][,cols2zero,mkr] = 0
+      # If there are not columns ending in 'Y', then process all samples together.
+      if(length(y_cols) == 0 ) {
       
-      # Renormalize to make the sum of each row at this marker equal to 1.
-      probs[[chr]][,,mkr] = probs[[chr]][,,mkr] / rowSums(probs[[chr]][,,mkr])
+        probs[[chr]] = clean_one_chr(probs[[chr]], thr)       
+      
+      } else {
 
-    } # for(mkr)
+        # Process males and females separately.
+        y_sums = rowMeans(probs[[chr]][,y_cols,])
+      
+        # This needs to be tested more.
+        females = which(y_sums == 0)
+        
+        # If all or none of the samples are female, then process all samples together.
+        if(length(females) == 0 | length(females) == nrow(probs[[chr]])) {
+        
+          probs[[chr]] = clean_one_chr(probs[[chr]], thr)
+        
+        } else {
+        
+          # Process each sex separately.
+          males = setdiff(1:nrow(probs[[chr]]), females)
+          
+          probs[[chr]][females,,] =  clean_one_chr(probs[[chr]][females,,], thr)
+          probs[[chr]][males,,]   =  clean_one_chr(probs[[chr]][males,,],   thr)
+        
+        } # else
+      
+      } # else
+      
+    } else {
+
+      probs[[chr]] = clean_one_chr(probs[[chr]], thr)
+
+    } # else
 
   } # for(chr)
   
@@ -57,16 +107,24 @@ clean_genoprobs_by_col = function(probs, thr = 1.0) {
 
 
 # Test code:
-#probs_file = '/projects/korstanje-lab/Pureplex/AnalyzedData/TumorStudy_combined/results/quilt/20250421_tumorstudy_combined/2000/geno_probs/complete_genoprobs.rds'
-#probs = readRDS(probs_file)
+probs_file = '/projects/korstanje-lab/Pureplex/AnalyzedData/TumorStudy_combined/results/quilt/20250421_tumorstudy_combined/2000/geno_probs/complete_genoprobs.rds'
+probs = readRDS(probs_file)
 
-#new_probs = clean_genoprobs_by_col(probs, 20)
+new_probs = clean_genoprobs_by_col(probs, 20)
 
 # Check Chr each chromosome. This should be 1, 1.
-#for(i in seq_along(probs)) {
+for(i in seq_along(probs)) {
 
-#  print(paste('CHR:', i, ' : ', range(apply(probs[[i]], 3, rowSums))))
+  print(paste('CHR:', i, ' : ', range(apply(probs[[i]], 3, rowSums))))
 
-#} # for(i)
+} # for(i)
+
+for(i in seq_along(probs)) {
+
+   cs = apply(probs[[i]][,c('AC', 'AD', 'BC', 'BD'),], 2:3, sum)
+
+   print(range(apply(cs, 2, min)))
+
+} # for(i)
 
 
